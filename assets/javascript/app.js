d88f54bc120e0f63778c09a64121e88772eba6c4
Aaron Lee <worldedit05@gmail.com>
2016-09-02 21:59:31 -0500

    Fixed counter to start and stop with the questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,34 @@
 var answer;
 var questionNumber = 0;
 var time = 60;
+var counter;
 
 var quiz = [{
       question: "This is question #1",
       choices: ["choice1", "choice2","choice3"],
       rightAnswer: 2
-             }]
-
-var timer = setInterval(timeLeft, 1000);
+            },
+            {
+      question: "This is question #2",
+      choices: ["choice1", "choice2","choice3"],
+      rightAnswer: 3
+            }]
 
 nextQuestion();
 
-$('.answer').on('click', function() {
+
+$(document).on('click', '.answer', function() {
   clearInterval(timeLeft);
+  stop();
   answer = $(this).attr('id');
   if (checkAnswer(answer)) {
     $('#question').html("Correct!");
+
   }else {
     $('#question').html("Sorry :(");
   }
-  clearInterval(timer);
+  questionNumber++;
+  setTimeout(nextQuestion, 5*1000);
 });
 
 function timeLeft() {
@@ -33,12 +41,20 @@ function timeLeft() {
     $('#timer').html('Times up!');
   }
 }
+function start() {
+  counter = setInterval(timeLeft, 1000);
+}
+function stop() {
+  clearInterval(counter);
+}
 function createQuestion(index) {
   $('#question').html(quiz[index].question);
 }
 function createChoices(index) {
+  var b = $('#multipleChoice');
+  b.empty();
   for ( var i = 0; i < quiz[index].choices.length; i++ ) {
-    $('#multipleChoice').append('<li id="' + (i + 1) +'" class="answer">' + quiz[index].choices[i]);
+    b.append('<li id="' + (i + 1) +'" class="answer">' + quiz[index].choices[i]);
   }
 }
 function nextQuestion() {
@@ -46,8 +62,9 @@ function nextQuestion() {
     var nextQuestion = createQuestion(questionNumber);
     var nextChoiceSet = createChoices(questionNumber);
   }
+  clearTimeout(nextQuestion);
+  start();
 }
-// TODO: answer checking function
 function checkAnswer(userAnswer) {
   if (userAnswer == quiz[questionNumber].rightAnswer) {
     console.log("Correct!")
